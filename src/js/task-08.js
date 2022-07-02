f24de6296708d33c93f6d1dd92313afdbb3901d9
Lora <lorat1403@gmail.com>
2022-07-02 19:34:08 +0300

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,22 +1,22 @@
-// const loginForm = document.querySelector(".login-form");
+const loginForm = document.querySelector(".login-form");
 
-// const chekLoginForm = (event) => {
-//   event.preventDefault();
+const chekLoginForm = (event) => {
+  event.preventDefault();
 
-//   const email = event.currentTarget.elements.email.value;
-//   const password = event.currentTarget.elements.password.value;
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
 
-//   if (email === "" || password === "") {
-//     return alert("Все поля должны быть заполнены");
-//   } else {
-//     const formData = {
-//       email,
-//       password,
-//     };
-//     console.log(formData);
-//   }
+  if (email === "" || password === "") {
+    return alert("Все поля должны быть заполнены");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+  }
 
-//   return loginForm.reset();
-// };
+  return loginForm.reset();
+};
 
-// loginForm.addEventListener("submit", chekLoginForm);
+loginForm.addEventListener("submit", chekLoginForm);
