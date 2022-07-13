1c825583d73448270a0cd26360bf3e875d0ca8c2
Lora <lorat1403@gmail.com>
2022-07-13 20:27:00 +0300

    updtask-1

--- js/task-01.js
+++ js/task-01.js
@@ -2,18 +2,7 @@ const items = document.querySelectorAll(".item");
 
 console.log(`Numbers of categories : ${items.length}`);
 
-const titles = document.querySelectorAll("h2");
-
-const list = document.querySelectorAll(".item > ul");
-const sublist1 = list[0].querySelectorAll("li");
-const sublist2 = list[1].querySelectorAll("li");
-const sublist3 = list[2].querySelectorAll("li");
-
-console.log(`Category : ${titles[0].innerHTML}`);
-console.log(`Elements : ${sublist1.length}`);
-
-console.log(`Category : ${titles[1].innerHTML}`);
-console.log(`Elements : ${sublist2.length}`);
-
-console.log(`Category : ${titles[2].innerHTML}`);
-console.log(`Elements : ${sublist3.length}`);
+[...items].forEach((element) => {
+  console.log("Category :", element.firstElementChild.textContent);
+  console.log("Elements :", element.lastElementChild.children.length);
+});
