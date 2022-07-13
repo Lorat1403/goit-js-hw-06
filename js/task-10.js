c074cb8ed111aa13045f036bcad49b65b3f41693
Lora <lorat1403@gmail.com>
2022-07-13 21:01:17 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputRef = document.querySelector("[type=number]");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const arrayDiv = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  for (let i = 0; i < amount; i += 1) {
+    const widthHeightSize = 30 + i * 10 + "px";
+    const divNew = document.createElement("div");
+    divNew.style.width = widthHeightSize; // ширина
+    divNew.style.height = widthHeightSize; // высота
+    divNew.style.backgroundColor = getRandomHexColor();
+    divNew.style.marginTop = 5 + "px";
+    arrayDiv.append(divNew);
+  }
+};
+
+function onCreateClick(el) {
+  arrayDiv.innerHTML = "";
+  createBoxes(inputRef.value);
+}
+
+function onDestroyClick(el) {
+  arrayDiv.innerHTML = "";
+  inputRef.value = 0;
+}
+createBtn.addEventListener("click", onCreateClick);
+destroyBtn.addEventListener("click", onDestroyClick);
